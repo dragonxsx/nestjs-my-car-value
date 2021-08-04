101c2a592e342d00c4b2795c9e9a76e63e590e6a
LongNT <longnt@ql6625.fr>
2021-08-04 10:47:36 +0700

    Updating data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,4 +20,16 @@ export class UsersService {
     find(email: string) {
         return this.repo.find({email});
     }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+
+        if(!user) {
+            throw new Error('user not found');
+        }
+        
+        Object.assign(user, attrs);
+
+        return this.repo.save(user);
+    }
 }
