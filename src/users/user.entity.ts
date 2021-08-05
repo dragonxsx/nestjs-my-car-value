5bb2099a8f7acb7acd6f05f987c0a28ae7548087
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-05 23:26:51 +0700

    Use ClassSerializerInterceptor to exclude password

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from "class-transformer";
 import {Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
 
 @Entity()
@@ -9,6 +10,7 @@ export class User {
     email: string;
 
     @Column()
+    @Exclude()
     password: string;
 
     @AfterInsert()
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
-import { get } from 'http';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -13,6 +12,7 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
       const user = await this.usersService.findOne(parseInt(id));
