c14d087cff5b14202fdb832c513f1d482c5fd842
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-06 16:04:47 +0700

    Setting up globally scoped interceptor

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,18 @@ import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor]
+  providers: [
+    UsersService, 
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor
+    }
+  ]
 })
 export class UsersModule {}
