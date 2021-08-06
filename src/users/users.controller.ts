2a27b24696100574369b072fe56de831e6635a7a
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-06 14:46:41 +0700

    Get current user information

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,20 +14,23 @@ export class UsersController {
         private authService: AuthService    
     ) {}
 
-    @Post('/colors/:color')
-    setColor(@Param('color') color: string, @Session() session: any) {
-        session.color = color;
-    }
-
-    @Get('/colors')
-    getColor(@Session() session: any) {
-        return session.color;
-    }
-
-
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Post('/signin')
+    async signIn(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userId = user.id;
+        return user;
+    }
+
+    @Get('/me')
+    async whoAmI(@Session() session: any) {
+        return this.usersService.findOne(session.userId);
     }
 
     @Post('/signin')
