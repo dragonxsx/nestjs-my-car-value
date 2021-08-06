1f0a1db385f4831d772a6a10b0052801430f1bf5
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-06 11:53:17 +0700

    Implementing Sign in functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { randomBytes, scrypt as _scrypt } from "crypto";
 import { promisify } from "util";
@@ -32,6 +32,23 @@ export class AuthService {
         const user = this.usersService.create(email, result);
 
         // Return the user
+        return user;
+    }
+
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if(!user) {
+            throw new NotFoundException('user not found');
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        
+        if(storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('bad password');
+        }
+
         return user;
     }
 }
