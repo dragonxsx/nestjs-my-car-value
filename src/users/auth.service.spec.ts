4c1dc09f776f839af5e4fb169c77fe70c24b7f6d
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-09 23:45:42 +0700

    Add type check to mocks

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
 it('can create an instance of auth service', async () => {
     // Create a fake copy of users service
-    const fakeUsersService = {
+    const fakeUsersService : Partial<UsersService> = {
         find: () => Promise.resolve([]),
-        create: (email: string, password: string) => Promise.resolve({id: 1, email, password})
+        create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
     };
 
     const module = await Test.createTestingModule({
