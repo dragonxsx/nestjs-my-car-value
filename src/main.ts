84bb7982a05148f5d464a8317edec3080a8f4da4
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-10 22:27:08 +0700

    Revert previous refactoring for another solution

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import {setupApp} from './setup-app'
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  setupApp(app);
- 
+  app.use(cookieSession({
+    keys: ['asdasddf']
+  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  );
+
   await app.listen(3000);
 }
 bootstrap();
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { setupApp } from '../src/setup-app';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,7 +12,6 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupApp(app);
     await app.init();
   });
 
