2884bb37dda6611330ef3ed46b3ec48ff3390d1e
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-13 16:25:50 +0700

    Adding in Report Approval

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
 import { Repository } from 'typeorm';
@@ -15,6 +15,17 @@ export class ReportsService {
         const report  = this.repo.create(reportDto);
         report.user = user;
         
+        return this.repo.save(report);
+    }
+
+    async changeApproval(id: string, approved: boolean) {
+        const report = await this.repo.findOne(id);
+
+        if(!report) {
+            throw new NotFoundException('report not found');
+        }
+
+        report.approved = approved;
         return this.repo.save(report);
     }
 }
