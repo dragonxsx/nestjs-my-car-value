fa08a9e7cf0eae7c442370ccb375a3467e6377ed
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-15 12:04:46 +0700

    Getting COOKIE_KEY from env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -48,10 +48,14 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(
+    private configService: ConfigService
+  ) {}
+  
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(cookieSession({
-        keys: ['asdaskdfj']
+        keys: [this.configService.get('COOKIE_KEY')]
       }))
       .forRoutes('*');
   }
