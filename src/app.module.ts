238ca11e563977c4e8bf303c1e003f114b62e6f3
LongNT <thanhlong123_vietnam@yahoo.com.vn>
2021-08-16 09:24:50 +0700

    Creating env-specific database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,23 +16,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV}.env`
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: true,
-          entities: [User, Report]
-        }
-      }
-    }),
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: process.env.NODE_ENV === 'test' ? 'test.sqlite' : 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true
-    // }),
+    TypeOrmModule.forRoot(),
     UsersModule, 
     ReportsModule
   ],
